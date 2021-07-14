5334d6188a2ec28367dc00ea2335a63dd3b5c92a
dan yang <yangdan1357@gmail.com>
2021-07-14 19:47:57 +0800

    add new file

--- leetcode/2021/3.无重复字符的最长子串.js
+++ leetcode/2021/3.无重复字符的最长子串.js
@@ -16,19 +16,25 @@
  * 需要输出字符串的长度
  */
 var lengthOfLongestSubstring = function(s) {
+  // 使用set来检测是否有重复的字符
   const occ = new Set();
   const len = s.length;
+  // rk是右指针，初始值为-1，还没开始移动
   let rk = -1, ans = 0;
   for (let i = 0; i < len; i++) {
+    // 每循环一次就会有一个重复的字符串，删掉最左边的字符
     if (i !== 0) {
       occ.delete(s.charAt(i - 1))
     }
+    // 从当前字符开始向右累加，获取最长不重复的字符串
     while (rk + 1 < len && !occ.has(s.charAt(rk + 1))) {
       occ.add(s.charAt(rk + 1));
       ++rk;
     }
+    // 获取最长字符的长度
     ans = Math.max(ans, rk - i + 1)
   }
+  return ans
 };
 // @lc code=end
 
---
+++ leetcode/2021/4.寻找两个正序数组的中位数.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=javascript
+ *
+ * [4] 寻找两个正序数组的中位数
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+  let len1 = nums1.length
+  let len2 = nums2.length
+  if (nums1.length === 0 && nums2.length > 0) {
+    return 
+  }
+};
+// @lc code=end
+
